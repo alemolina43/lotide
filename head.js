ab971db60f002575f55ff421f18e0f2cb0053857
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-17 20:49:34 -0800

    Refactor function to add AI feedback recommendations

--- head.js
+++ head.js
@@ -6,14 +6,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Will return the first element of an array
 const head = function(arr) {
   return arr[0];
 };
 
-
 //TEST CODE
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([4, 5, 7, 8, 9]), 7);
+assertEqual(head([4, 5, 7, 8, 9]), 4);
 console.log(head([2]));
 console.log(head([]));
