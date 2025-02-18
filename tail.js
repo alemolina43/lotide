37de312921bdbe265ac07251a939dd8a1cadab54
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-17 21:17:59 -0800

    Create a tail function to return all the elements of an array but the head

---
+++ tail.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
+assertEqual(result.length, 2); //will return true
+assertEqual(result[0], "Lighthouse"); // will return true
+assertEqual(result[1], "Labs");
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+const emptyArr = [];
+console.log(tail(emptyArr));
+const oneElement = [9];
+console.log(tail(oneElement));
