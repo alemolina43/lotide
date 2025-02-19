cb0856c7138b11cc66e5e2dff69a9b1d480d0b98
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-18 20:30:50 -0800

    Add more test cases based on AI feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,7 +14,8 @@ const assertArraysEqual = function(firstArr, secondArr) {
 
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], [1, 2, "3"]);
-assertArraysEqual(["3", "2", "1"], ["3", "2", "1"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); //should fail
+assertArraysEqual(["1", "2", "3"], [1, 2, "3"]); //should fail
+assertArraysEqual(["3", "2", "1"], ["3", "2", "1"]); //should pass
+assertArraysEqual([1, 2], [1, 2, 3]); //should fail
