f38770960891b5421e5e916984a63ad37a4e8591
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-18 19:10:26 -0800

    Change test code based on AI review

--- eqArrays.js
+++ eqArrays.js
@@ -18,12 +18,6 @@ const eqArrays = function(arr1, arr2) {
 
 
 //test code
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
