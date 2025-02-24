f971f05486c4d8c907aecdbe1c4ad45dbd8757fe
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-24 12:37:57 -0800

    Fix test case

--- test/headTest.js
+++ test/headTest.js
@@ -10,8 +10,8 @@ describe("#head", () => {
     assert.strictEqual(head(['5']), '5');
   });
 
-  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs'']", () => {
-    assert.strictEqual(head(['Hello']), 'Hello');
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
 
   it("returns 4 for [4, 5, 7, 8, 9]", () => {
