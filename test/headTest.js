d59810d54e27a283bfda33533011250c1fb90cda
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-24 12:35:35 -0800

    Refactor headTest to use mocha and chai, add .gitignore file and create json files

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,29 @@
-const head = require("../head");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const head   = require('../head');
 
-//TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([4, 5, 7, 8, 9]), 4);
-assertEqual(head([2]), 2); //should PASS
-assertEqual(head([]), undefined); //should PASS
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs'']", () => {
+    assert.strictEqual(head(['Hello']), 'Hello');
+  });
+
+  it("returns 4 for [4, 5, 7, 8, 9]", () => {
+    assert.strictEqual(head([4, 5, 7, 8, 9]), 4);
+  });
+
+  it("returns 2 for [2]", () => {
+    assert.strictEqual(head([2]), 2);
+  });
+
+  it("returns undefined for []]", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
+
