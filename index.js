c71c20c5aa66f931191da473fcca7277102269cb
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-24 13:37:24 -0800

    Add missing functions and fix typos

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ const middle = require('./middle');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const findKey = require('./findKey');
-const findkeyByValue = require('./findKeyByValue');
+const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
-const takeUnit = require('./takeUntil');
+const takeUntil = require('./takeUntil');
 const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const asserObjectsEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findkeyByValue: findkeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUnit: takeUnit,
-  without: without
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  assertArraysEqual,
+  assertEqual,
+  asserObjectsEqual,
+  eqArrays,
+  eqObjects
 };
